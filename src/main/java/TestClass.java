dd014ae95edf4e2b5a4a01b2936bd7782f52efc4
apriebe <apriebe@dxc.com>
2020-07-28 14:06:38 +0200

    Initial commit

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,17 @@
+/**
+ * == Some asciidoc
+ *
+ * A list:
+ *
+ * . Item
+ * . Item
+ * . Item
+ *
+ * [NOTE]
+ * It worked!
+ *
+ * include::{templatedir}/fields/_test.adoc[]
+ */
+public class TestClass {
+
+}
